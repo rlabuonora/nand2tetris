45f42f5ee1f38bcc5625dbc4046a4a3f8c9e306c
Rafael La Buonora <rlabuonora@yahoo.com>
2017-01-13 17:23:22 -0300

    Preprocess files for comments and whitespace

--- 06/assembler/test/test.js
+++ 06/assembler/test/test.js
@@ -1,19 +1,19 @@
 var assert = require('assert');
-var preprocess = require('../assembler.js').preprocess;
+var assembler = require('../assembler.js');
 
 
 describe('Regular expression', function() {
 
     describe('removing comments', function() {
 
-        var re = /\/\/.*$/;
+        var re = /\/\/.*\r/;
 
         it('full line comment', function() {
-            var str = "// this is a comment";
+            var str = "// this is a comment\r";
             assert.equal(str.replace(re, ""), "");
         });
         it('a comment after an instruction', function() {
-            var str = "ADD 1 + 2 // this is a comment";
+            var str = "ADD 1 + 2 // this is a comment\r";
             assert.equal(str.replace(re, ""), "ADD 1 + 2 ");
         });
     });
@@ -23,22 +23,27 @@ describe('Regular expression', function() {
 describe('Removing whitespace', function() {
 
     it('blank line', function() {
-        var str = "      ";
+        var str = "      \r";
         assert.equal(str.trim(), "");
     });
 
     it('Leading whitespace', function() {
-        var str = "   ADD 1 + 2";
+        var str = "   ADD 1 + 2\r";
         assert.equal(str.trim(), "ADD 1 + 2");
     });
 
     it('Trailing whitespace', function() {
-        var str = "ADD 1 + 2    ";
+        var str = "ADD 1 + 2    \r";
         assert.equal(str.trim(), "ADD 1 + 2");
     });
 
     it('Leading & trailing whitespace', function() {
-        var str = "   ADD 1 + 2    ";
+        var str = "   ADD 1 + 2    \r";
+        assert.equal(str.trim(), "ADD 1 + 2");
+    });
+
+    it('line with carriage return', function() {
+        var str = "   ADD 1 + 2\r";
         assert.equal(str.trim(), "ADD 1 + 2");
     });
 });
@@ -47,30 +52,50 @@ describe('Removing whitespace', function() {
 describe('Removing whitespace and comments', function() {
 
     it('an empty line', function() {
-        var str = "";
-        assert.equal(preprocess(str), "");
+        var str = "\r";
+        assert.equal(assembler.preprocessLine(str), "");
     });
 
     it('a blank line', function() {
-        var str = "      ";
-        assert.equal(preprocess(str), "");
+        var str = "      \r";
+        assert.equal(assembler.preprocessLine(str), "");
     });
 
     it('an instruction ', function() {
-        var str = "ADD 1 + 2";
-        assert.equal(preprocess(str), "ADD 1 + 2");
+        var str = "ADD 1 + 2\r";
+        assert.equal(assembler.preprocessLine(str), "ADD 1 + 2");
     });
 
 
     it('an instruction with a comment', function() {
-        var str = "ADD 1 + 2  // This is a comment";
-        assert.equal(preprocess(str), "ADD 1 + 2");
+        var str = "ADD 1 + 2  // This is a comment\r";
+        assert.equal(assembler.preprocessLine(str), "ADD 1 + 2");
     });
 
     it('a comment', function() {
-        var str = "// This is a comment";
-        assert.equal(preprocess(str), "");
+        var str = "// This is a comment\r";
+        assert.equal(assembler.preprocessLine(str), "");
     });
 });
 
 
+describe("Reading a file", function() {
+    it('reads the file', function() {
+        var lines = assembler.readFile('../add/Add.asm');
+        var first = "// This file is part of www.nand2tetris.org\r"
+        assert.equal(lines.length, 13);
+        assert.equal(lines[0], first);
+    });
+});
+
+
+describe("A collection of lines", function() {
+    it("preprocess", function() {
+        var path = '../max/Max.asm';
+        var lines = assembler.preprocessFile(path);
+        assert.equal(lines[0], "");
+        assert.equal(lines[7], "@R0");
+        assert.equal(lines[8], "D=M");
+        
+    });
+});
