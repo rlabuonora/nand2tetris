b7a5bb2b5a234224db3230b67f4325fa59605a8f
Rafael La Buonora <rlabuonora@yahoo.com>
2017-01-16 14:45:52 -0300

    Use module pattern for parser

--- 06/assembler/parser.js
+++ 06/assembler/parser.js
@@ -1,19 +1,22 @@
-exports.preprocessLine = function(str) {
-    var re = /\/\/.*\r/;
-    var newLine = str.replace(re, "").trim();
-    return newLine;
+var parser = {
+    preprocessLine:  function(str) {
+        var re = /\/\/.*\r/;
+        var newLine = str.replace(re, "").trim();
+        return newLine;
+    },
+
+    readFile: function(file) {
+        // return an array with the lines of the file
+        var fs = require('fs');
+        return fs.readFileSync(file).toString().split("\n").slice(0, -1); // remove last blank line
+    },
+
+    preprocessFile:  function(file) {
+        var arr = this.readFile(file);
+        return arr.map(function(line) {
+            return this.preprocessLine(line)
+        }.bind(this));
+    }
 };
 
-exports.readFile = function(file) {
-    // return an array with the lines of the file
-    var fs = require('fs');
-    return fs.readFileSync(file).toString().split("\n").slice(0, -1); // remove last blank line
-
-};
-
-exports.preprocessFile = function(file) {
-    var arr = this.readFile(file);
-    return arr.map(function(line) {
-        return this.preprocessLine(line)
-    }.bind(this));
-};
+module.exports = parser;
