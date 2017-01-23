d3e6e9734f6f2cc0b4f62be5c9d477b6e3499e63
Rafael La Buonora <rlabuonora@yahoo.com>
2017-01-23 13:43:54 -0300

    Finish file managing for VMTranslator

--- 07/VMTranslator/VMTranslator.js
+++ 07/VMTranslator/VMTranslator.js
@@ -5,33 +5,40 @@ function VMTranslator( sourcePath ) {
     this.files;
     this.objectFileName;
     this.destinationPath;
-    
+    // Helpers
+    this.getObjectFileNameFromPath = function ( sourcePath ) {
+        return sourcePath.substr(sourcePath.lastIndexOf("/")+1, sourcePath.length) +".asm";
+    };
+    this.getObjectFileNameFromFile = function ( sourcePath ) {
+        return path.basename(sourcePath, '.vm') + ".asm";
+    };
+    this.saveFile = function( assemblerCode ) {
+        fs.writeFile(this.destinationPath + "/" + this.objectFileName, assemblerCode , function (err) {
+            if (err) return console.log(err);
+        });        
+    };
+
+    this.removeTrailingSlash = function (sourcePath ) {
+        if (sourcePath.slice(-1) === "/") return sourcePath.slice(0,-1);
+        else return sourcePath;
+    };
+
+    var sourcePath = this.removeTrailingSlash( sourcePath );
+    // constructor code
     var stats = fs.statSync( sourcePath );
     // if is directory
     if (stats.isDirectory()) {
-        this.files = fs.readdirSync( sourcePath ).map(function(file) { return sourcePath + file; });
+        this.files = fs.readdirSync( sourcePath ).map(function(file) { return sourcePath + "/" + file; });
         this.destinationPath = sourcePath;
-        var fileName = getObjectFileNameFromPath( sourcePath );
-    }
+        this.objectFileName = this.getObjectFileNameFromPath( sourcePath );
+    } // if is a single file
     else if (stats.isFile()) {
         this.files = [ sourcePath ];
         this.destinationPath = path.dirname( sourcePath );
-        this.objectFileName = getObjectFileNameFromFile( sourcePath );
+        this.objectFileName = this.getObjectFileNameFromFile( sourcePath );
     }
 
-    function saveFile( assemblerCode ) {
-        fs.writeFile(this.objectFileName, assemblerCode , function (err) {
-            if (err) return console.log(err);
-        });        
-    }
-    // if is a single file
-    function getObjectFileNameFromFile( sourcePath ) {
-        return "output.asm";
-    }
-    function getObjectFileNameFromPath( sourcePath ) {
-        return "output.asm";
-    }
-
+    //this.saveFile("assembler commands");
 }
 
 module.exports = VMTranslator;
--- 07/VMTranslator/test/test.js
+++ 07/VMTranslator/test/test.js
@@ -9,6 +9,7 @@ describe('VMTranslator', function() {
             var t1 = new VMTranslator( file );
             assert.deepEqual(t1.files, ['./test/support/singleFile/singleFile.vm']);
             assert.equal(t1.destinationPath, './test/support/singleFile');
+            assert.equal(t1.objectFileName, 'singleFile.asm');
             
         });
         it('should throw an error if the file/dir does not exist', function() {
@@ -20,16 +21,33 @@ describe('VMTranslator', function() {
             assert.throws(function () { new VMTranslator( file ); }, Error);
         });
     });
+    describe('remove trailing slash', function() {
+
+        it("with trailing slash", function() {
+            var file = './test/support/multipleFiles/';
+            var t1 = new VMTranslator( file );
+            assert.equal(t1.removeTrailingSlash(file), './test/support/multipleFiles');
+        });
+        it("without trailing slash", function() {
+            var file = './test/support/multipleFiles';
+            var t1 = new VMTranslator( file );
+            assert.equal(t1.removeTrailingSlash(file), './test/support/multipleFiles');
+        });
+     
+        
+    });
     describe('translate a folder', function() {
         it('should create a file', function() {
             // choose asm filename
-            var file = './test/support/multipleFiles/';
+            var file = './test/support/multipleFiles';
             var t1 = new VMTranslator( file );
             assert.deepEqual(t1.files, [
                 './test/support/multipleFiles/firstFile.vm',
                 './test/support/multipleFiles/secondFile.vm'
             ]);
             assert.equal(t1.destinationPath, file);
+            assert.equal(t1.objectFileName, 'multipleFiles.asm');
+            // assert(fs.existsSync('./test/support/multipleFiles/multipleFiles.asm'));
         });
         // how do I test that I have read all the files in the folder
         // TODO use translation to make sure we translate all of the files
