cc8ba8aa9b18c607f7b6baf2253349fd8b9d712b
Rafael La Buonora <rlabuonora@yahoo.com>
2017-01-31 15:30:57 -0300

    Testing

---
+++ 08/ProgramFlow/BasicLoop/BasicLoop.asm
@@ -0,0 +1,155 @@
+// push constant 0
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop local 0
+@0
+D=A
+@LCL
+D=D+M
+@addr
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@addr
+A=M
+M=D
+// label LOOP_START
+(LOOP_START)
+// push argument 0
+@0
+D=A
+@ARG
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push local 0
+@0
+D=A
+@LCL
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@SP
+M=M-1
+A=M
+D=D+M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop local 0
+@0
+D=A
+@LCL
+D=D+M
+@addr
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@addr
+A=M
+M=D
+// push argument 0
+@0
+D=A
+@ARG
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+@SP
+A=M
+D=-M
+@SP
+M=M-1
+A=M
+D=D+M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop argument 0
+@0
+D=A
+@ARG
+D=D+M
+@addr
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@addr
+A=M
+M=D
+// push argument 0
+@0
+D=A
+@ARG
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// if-goto LOOP_START
+@SP
+M=M-1
+A=M
+D=M
+@LOOP_START
+D;JGT
+// push local 0
+@0
+D=A
+@LCL
+A=D+M
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
